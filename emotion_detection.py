b50dbbda9ebd6a2f7be8888d354cb754ca61d501
Luis Felipe <99270381+luisfelipeibanez@users.noreply.github.com>
2026-06-23 11:10:38 -0500

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 def emotion_detector(text_to_analyze):
@@ -6,8 +7,10 @@ def emotion_detector(text_to_analyze):
     Analyze emotions in text using Watson NLP.
     """
 
-    url = ('https://sn-watson-emotion.labs.skills.network/v1/'
-           'watson.runtime.nlp.v1/NlpService/EmotionPredict')
+    url = (
+        "https://sn-watson-emotion.labs.skills.network/v1/"
+        "watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    )
 
     headers = {
         "grpc-metadata-mm-model-id":
@@ -26,4 +29,23 @@ def emotion_detector(text_to_analyze):
         headers=headers
     )
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion
+    }
